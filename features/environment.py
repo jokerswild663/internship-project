22dced0b3552a12684d3cbc962864105bf09f4d2
Jeff Wyckoff <jokerswild663@gmail.com>
2025-07-19 01:18:46 -0700

    QAAR-859 updating selectors and adding arguments

--- features/environment.py
+++ features/environment.py
@@ -8,27 +8,27 @@ def browser_init(context):
     :param context: Behave context
     """
     ## chrome
-    options=webdriver.ChromeOptions()
-    options.add_argument('--headless=new')
-    options.add_argument('--window-size=1920,1080')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-web-security')
-    options.add_argument('--allow-running-insecure-content')
-    options.add_argument('--disable-blink-features=AutomationControlled')
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option('useAutomationExtension', False)    # options.add_argument('--remote-debugging-port=9222')
-    options.add_argument('--remote-debugging-port=9222')
-
-    context.driver = webdriver.Chrome(
-        options=options,
-    )
+    # options=webdriver.ChromeOptions()
+    # options.add_argument('--headless=new')
+    # options.add_argument('--window-size=1920,1080')
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-dev-shm-usage')
+    # options.add_argument('--disable-gpu')
+    # options.add_argument('--disable-web-security')
+    # options.add_argument('--allow-running-insecure-content')
+    # options.add_argument('--disable-blink-features=AutomationControlled')
+    # options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36")
+    # options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    # options.add_experimental_option('useAutomationExtension', False)    # options.add_argument('--remote-debugging-port=9222')
+    # options.add_argument('--remote-debugging-port=9222')
+    #
+    # context.driver = webdriver.Chrome(
+    #     options=options,
+    # )
 
 
     ## firefox
-    # context.driver = webdriver.Firefox()
+    context.driver = webdriver.Firefox()
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(20)
