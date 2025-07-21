691136514cccf949cb466a8e882f1d04d01379b9
Jeff Wyckoff <jokerswild663@gmail.com>
2025-07-21 11:50:19 -0700

    QAAR-828 fixing element lookup and verification

--- pages/base.py
+++ pages/base.py
@@ -14,17 +14,22 @@ class Base:
         element = self.driver.wait.until(EC.presence_of_element_located(locator))
         return element
 
+    def get_elements(self, locator):
+        self.driver.wait.until(EC.presence_of_element_located(locator))
+        elements = self.driver.find_elements(*locator)
+        return elements
+
     def input_field(self, locator, text):
         self.driver.wait.until(EC.visibility_of_element_located(locator)).send_keys(text)
 
     def click_element(self,locator):
         self.driver.wait.until(EC.element_to_be_clickable(locator)).click()
 
-    def verify_text(self, text,locator):
+    def verify_text(self, entries, locator):
         actual=self.driver.wait.until(EC.visibility_of_element_located(locator)).text
-        expected=text
+        expected=entries
 
-        assert actual==expected, f'actual: {actual} != expected: {expected}'
+        assert actual in expected, f'actual: {actual} Not in expected: {expected}'
 
     def verify_clickable(self,locator):
         assert EC.element_to_be_clickable(locator), f'{locator.text} is not clickable'
